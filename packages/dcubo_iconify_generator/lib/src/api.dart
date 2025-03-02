af45a86b600e8ba1a9130ec7e0cb2e7b2d7b495f
rmortes <56652057+rmortes@users.noreply.github.com>
2025-03-02 22:29:51 +0100

    Partitioned classes to make them leaner in general and to satisfy github constraints

--- packages/dcubo_iconify_generator/lib/src/api.dart
+++ packages/dcubo_iconify_generator/lib/src/api.dart
@@ -61,10 +61,10 @@ class IconSetDataInfo {
     required this.name,
     required this.total,
     required this.authorName,
-    required this.authorUrl,
     required this.licenseTitle,
     required this.licenseSpdx,
     required this.palette,
+    this.authorUrl,
     this.licenseUrl,
     this.height,
     this.category,
@@ -80,7 +80,7 @@ class IconSetDataInfo {
       total: json['total'] as int,
       version: json['version'] as String?,
       authorName: json['author']['name'] as String,
-      authorUrl: json['author']['url'] as String,
+      authorUrl: json['author']['url'] as String?,
       licenseTitle: json['license']['title'] as String,
       licenseSpdx: json['license']['spdx'] as String,
       licenseUrl: json['license']['url'] as String?,
@@ -97,7 +97,7 @@ class IconSetDataInfo {
   final int total;
   final String? version;
   final String authorName;
-  final String authorUrl;
+  final String? authorUrl;
   final String licenseTitle;
   final String licenseSpdx;
   final String? licenseUrl;
@@ -260,11 +260,119 @@ class IconSetData {
       'icons': icons.map((e) => e.toMap()).toList(),
       'fileName': fileName,
       'className': className,
+      'numeralIcons': numeralIcons.map((e) => e.toMap()).toList(),
+      'startingAIcons': startingAIcons.map((e) => e.toMap()).toList(),
+      'startingBIcons': startingBIcons.map((e) => e.toMap()).toList(),
+      'startingCIcons': startingCIcons.map((e) => e.toMap()).toList(),
+      'startingDIcons': startingDIcons.map((e) => e.toMap()).toList(),
+      'startingEIcons': startingEIcons.map((e) => e.toMap()).toList(),
+      'startingFIcons': startingFIcons.map((e) => e.toMap()).toList(),
+      'startingGIcons': startingGIcons.map((e) => e.toMap()).toList(),
+      'startingHIcons': startingHIcons.map((e) => e.toMap()).toList(),
+      'startingIIcons': startingIIcons.map((e) => e.toMap()).toList(),
+      'startingJIcons': startingJIcons.map((e) => e.toMap()).toList(),
+      'startingKIcons': startingKIcons.map((e) => e.toMap()).toList(),
+      'startingLIcons': startingLIcons.map((e) => e.toMap()).toList(),
+      'startingMIcons': startingMIcons.map((e) => e.toMap()).toList(),
+      'startingNIcons': startingNIcons.map((e) => e.toMap()).toList(),
+      'startingOIcons': startingOIcons.map((e) => e.toMap()).toList(),
+      'startingPIcons': startingPIcons.map((e) => e.toMap()).toList(),
+      'startingQIcons': startingQIcons.map((e) => e.toMap()).toList(),
+      'startingRIcons': startingRIcons.map((e) => e.toMap()).toList(),
+      'startingSIcons': startingSIcons.map((e) => e.toMap()).toList(),
+      'startingTIcons': startingTIcons.map((e) => e.toMap()).toList(),
+      'startingUIcons': startingUIcons.map((e) => e.toMap()).toList(),
+      'startingVIcons': startingVIcons.map((e) => e.toMap()).toList(),
+      'startingWIcons': startingWIcons.map((e) => e.toMap()).toList(),
+      'startingXIcons': startingXIcons.map((e) => e.toMap()).toList(),
+      'startingYIcons': startingYIcons.map((e) => e.toMap()).toList(),
+      'startingZIcons': startingZIcons.map((e) => e.toMap()).toList(),
     };
   }
 
   String get fileName => prefix.replaceAll('-', '_');
   String get className => info.name.replaceAll(' ', '');
+
+  List<IconData> get numeralIcons =>
+      icons.where((icon) => icon.name.startsWith(RegExp(r'[0-9]'))).toList();
+
+  List<IconData> get startingAIcons =>
+      icons.where((icon) => icon.name.startsWith('a')).toList();
+
+  List<IconData> get startingBIcons =>
+      icons.where((icon) => icon.name.startsWith('b')).toList();
+
+  List<IconData> get startingCIcons =>
+      icons.where((icon) => icon.name.startsWith('c')).toList();
+
+  List<IconData> get startingDIcons =>
+      icons.where((icon) => icon.name.startsWith('d')).toList();
+
+  List<IconData> get startingEIcons =>
+      icons.where((icon) => icon.name.startsWith('e')).toList();
+
+  List<IconData> get startingFIcons =>
+      icons.where((icon) => icon.name.startsWith('f')).toList();
+
+  List<IconData> get startingGIcons =>
+      icons.where((icon) => icon.name.startsWith('g')).toList();
+
+  List<IconData> get startingHIcons =>
+      icons.where((icon) => icon.name.startsWith('h')).toList();
+
+  List<IconData> get startingIIcons =>
+      icons.where((icon) => icon.name.startsWith('i')).toList();
+
+  List<IconData> get startingJIcons =>
+      icons.where((icon) => icon.name.startsWith('j')).toList();
+
+  List<IconData> get startingKIcons =>
+      icons.where((icon) => icon.name.startsWith('k')).toList();
+
+  List<IconData> get startingLIcons =>
+      icons.where((icon) => icon.name.startsWith('l')).toList();
+
+  List<IconData> get startingMIcons =>
+      icons.where((icon) => icon.name.startsWith('m')).toList();
+
+  List<IconData> get startingNIcons =>
+      icons.where((icon) => icon.name.startsWith('n')).toList();
+
+  List<IconData> get startingOIcons =>
+      icons.where((icon) => icon.name.startsWith('o')).toList();
+
+  List<IconData> get startingPIcons =>
+      icons.where((icon) => icon.name.startsWith('p')).toList();
+
+  List<IconData> get startingQIcons =>
+      icons.where((icon) => icon.name.startsWith('q')).toList();
+
+  List<IconData> get startingRIcons =>
+      icons.where((icon) => icon.name.startsWith('r')).toList();
+
+  List<IconData> get startingSIcons =>
+      icons.where((icon) => icon.name.startsWith('s')).toList();
+
+  List<IconData> get startingTIcons =>
+      icons.where((icon) => icon.name.startsWith('t')).toList();
+
+  List<IconData> get startingUIcons =>
+      icons.where((icon) => icon.name.startsWith('u')).toList();
+
+  List<IconData> get startingVIcons =>
+      icons.where((icon) => icon.name.startsWith('v')).toList();
+
+  List<IconData> get startingWIcons =>
+      icons.where((icon) => icon.name.startsWith('w')).toList();
+
+  List<IconData> get startingXIcons =>
+      icons.where((icon) => icon.name.startsWith('x')).toList();
+
+  List<IconData> get startingYIcons =>
+      icons.where((icon) => icon.name.startsWith('y')).toList();
+
+  List<IconData> get startingZIcons =>
+      icons.where((icon) => icon.name.startsWith('z')).toList();
 }
 
 /// Fetches the icon sets from the Github API
